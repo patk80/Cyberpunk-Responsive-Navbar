b4ab5dcc3cec80ba986841b0ff0e29272754c0bd
Pat Kelly <PatK80@gmail.com>
2020-08-16 14:24:34 -0400

    Hamburger to X JS toggle added

--- scripts.js
+++ scripts.js
@@ -1,7 +1,13 @@
 const toggleButton  = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks   = document.getElementsByClassName("navbar-links")[0];
 const hamburgerIcon = document.querySelector("#hamburger-icon");
+const bar1          = document.querySelector("#bar-1");
+const bar2          = document.querySelector("#bar-2");
+const bar3          = document.querySelector("#bar-3");
 
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
+  bar1.classList.toggle("bar-1-active");
+  bar2.classList.toggle("bar-2-active");
+  bar3.classList.toggle("bar-3-active");
 });
