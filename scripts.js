2eaa136a042cd8d379699c0b3cc1d96766201bb7
Pat Kelly <PatK80@gmail.com>
2021-01-04 19:46:51 -0500

    Condensing JS w/function - close navLinks on click

--- scripts.js
+++ scripts.js
@@ -4,10 +4,34 @@ const hamburgerIcon = document.querySelector("#hamburger-icon");
 const bar1          = document.querySelector("#bar-1");
 const bar2          = document.querySelector("#bar-2");
 const bar3          = document.querySelector("#bar-3");
+const home          = document.querySelector("#link-1");
+const portfolio     = document.querySelector("#link-2");
+const about         = document.querySelector("#link-3");
+const contact       = document.querySelector("#link-4");
 
 toggleButton.addEventListener("click", () => {
+  switchToggleStatus();
+});
+
+home.addEventListener("click", () => {
+  switchToggleStatus();
+});
+
+portfolio.addEventListener("click", () => {
+  switchToggleStatus();
+});
+
+about.addEventListener("click", () => {
+  switchToggleStatus();
+});
+
+contact.addEventListener("click", () => {
+  switchToggleStatus();
+});
+
+function switchToggleStatus() {
   navbarLinks.classList.toggle("active");
   bar1.classList.toggle("bar-1-active");
   bar2.classList.toggle("bar-2-active");
   bar3.classList.toggle("bar-3-active");
-});
+}
